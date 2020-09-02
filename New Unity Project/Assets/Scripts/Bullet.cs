6abf1031fbdca98393c433510607b80f2ef1c42a
Adrian1jr <47188608+Adrian1jr@users.noreply.github.com>
2020-09-02 18:51:48 -0500

    Todo lo que he hecho hasta ahora
    Gracias por su atencion.

---
+++ New Unity Project/Assets/Scripts/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    //variables
+    //esta nos sirve para la velocidad de la bala
+    public float speed = 2f;
+    //variable del vector para la direccion
+    public Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //variable temporal de tipo vector2 para el movimiento 
+        //vector normalizado con magnitud 1 multiplicado x el speed
+        Vector2 movement = direction.normalized * speed * Time.deltaTime;
+
+        //con esto le vamos a decir que frame tiene que tener
+        //mi posicion + la que tengo + la que me quieren mover 
+        //esto es un objeto
+        //transform eso es un vector de 2
+        //transform.position = new Vector2(transform.position.x + movement.x, transform.position.y + movement.y);
+
+        //el metodo translate es de unity para hacer el movimiento con 1 sola linea sin hacer todo lo de arriba
+        transform.Translate(movement);
+    }
+}
---
+++ New Unity Project/Assets/Scripts/TestingKeys.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingKeys : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Vector2 direction;
+    public float speed = 2f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Mouse
+        //cuando se ha pulsado un boton
+        // if (Input.GetMouseButtonDown(0))
+        // {
+        //     Debug.Log("button pressed");
+        // }
+        // //cuando esta pulsado un boton
+        // //si cambiamos el 0 por un 1 para click derecho
+        // //si es 2 es el click de enmedio del mouse
+        // if (Input.GetMouseButton(0))
+        // {
+        //     Debug.Log("button is pressed");
+        // }
+        // //cuando el boton se suelta
+        // if (Input.GetMouseButtonUp(0))
+        // {
+        //     Debug.Log("button released");
+        // }
+
+        //para tener control del teclado
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("derecha");  
+            Vector2 movement = direction.normalized * speed * Time.deltaTime;   
+            transform.Translate(movement);
+  
+        }
+
+         if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("izquierda");  
+            Vector2 movement = direction.normalized * speed * Time.deltaTime;   
+            transform.Translate(movement);
+            direction.x=0;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Debug.Log("brincar");  
+            Vector2 movement = direction.normalized * speed * Time.deltaTime;   
+            transform.Translate(movement); 
+        }
+
+         if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Debug.Log("brincar");  
+            Vector2 movement = direction.normalized * speed * Time.deltaTime;   
+            transform.Translate(movement);
+  
+        }
+
+        // 3. Getting axis for movement
+		float horizontal = Input.GetAxis("Horizontal"); // Values from -1f to 1f
+		float vertical = Input.GetAxis("Vertical"); 	// Values from -1f to 1f
+
+		if (horizontal < 0f || horizontal > 0f) {
+			Debug.Log("Horizontal axis is " + horizontal);
+		}
+		if (vertical < 0f || vertical > 0f) {
+			Debug.Log("Vertical axis is " + vertical);
+		}
+
+        //asi lo recomienda unity por que tiene preestablecidos dentro de el como por ejemeplo para los joysticks y eso en teoria lo veo como unviersal
+        // if (Input.GetKeyDown("jump"))
+        // {
+        //     Debug.Log("usando esto. puedo hacer que el jugador salte por ser el espacio");
+        // }
+    }
+}
